f293e3d0c32c08d3f195b1353e9c19618bc42aa5
Vikram Pawar <pawarvikram2004@gmail.com>
2025-10-31 13:40:18 +0530

    Completed Weather API integration

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-console.log("Weather App is ready!");
+const API_KEY = "ecbfeb6a79825fdfe9839b9cf485df9f";
 
-alert("Welcome to My Weather App 🌦️");
+async function getWeather(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
 
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+    console.log("Weather Data:", data); 
+
+  } catch (error) {
+    console.error("Error fetching weather data:", error.message);
+  }
+}
+
+getWeather("Mumbai");
